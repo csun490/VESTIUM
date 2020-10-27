28a37af07b34484c994ebc1079d32be5e6576c72
mimi schirm <mimischirm@gmail.com>
2020-10-27 15:51:45 -0700

    add images to collection view in table view

---
+++ VESTIUM/MENU/SlideInTransition/CustomCollectionViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  CollectionViewCell.swift
+//  SlideInTransition
+//
+//  Created by Mimi  on 10/22/20.
+//  Copyright © 2020 Murat Bekgi. All rights reserved.
+//
+
+import UIKit
+
+import UIKit
+class CustomCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var cellImageView: UIImageView!
+    
+    var cellImageName:String?
+    class var CustomCell : CustomCollectionViewCell {
+        let cell = Bundle.main.loadNibNamed("CustomCollectionViewCell", owner: self, options: nil)?.last
+        return cell as! CustomCollectionViewCell
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        self.backgroundColor = UIColor.red
+        
+    }
+    
+    func updateCellWithImage(name:String) {
+        self.cellImageName = name
+        self.cellImageView.image = UIImage(named: name)
+    }
+
+}
---
+++ VESTIUM/MENU/SlideInTransition/SlideInTransition/Menu/ClosetFiles/DetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DetailsViewController.swift
+//  SlideInTransition
+//
+//  Created by Mimi  on 10/22/20.
+//  Copyright © 2020 Murat Bekgi. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    var category: ImageCategory?
+    var imageName: String?
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var detailImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Category Detail"
+        self.updateViewControllerWithDetails()
+    }
+    
+    func updateViewControllerWithDetails() {
+        if let selCategory = self.category, let imageTitle = self.imageName {
+            self.descriptionLabel.text = selCategory.catDescription
+            self.detailImageView.image = UIImage(named: imageTitle)
+        }
+    }
+    
+}
