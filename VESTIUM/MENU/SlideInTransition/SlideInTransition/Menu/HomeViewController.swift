92081ec49526b20cb8eaf9f8f48d5fea6d9ca1ec
Cesar Barrera <cesarbarrera@Cesars-MacBook-Air.local>
2020-10-27 00:20:36 -0700

    New Progess to HomeViewController & Storyboard

--- VESTIUM/MENU/SlideInTransition/SlideInTransition/Menu/HomeViewController.swift
+++ VESTIUM/MENU/SlideInTransition/SlideInTransition/Menu/HomeViewController.swift
@@ -32,6 +32,12 @@ class HomeViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         menuViewController.transitioningDelegate = self
         present(menuViewController, animated: true)
     }
+    // save button
+    
+    @IBAction func Save_button(_ sender: Any) {
+   saveImage(imageName: "test.png")
+        
+    }
     
     // camera button
     @IBAction func cameraButton(_ sender: Any) {
@@ -42,6 +48,7 @@ class HomeViewController: UIViewController, UIImagePickerControllerDelegate, UIN
                imagePicker.allowsEditing = true
                present(imagePicker, animated: true, completion: nil)
         }
+        
     }
     
     // photo library button
@@ -54,15 +61,40 @@ class HomeViewController: UIViewController, UIImagePickerControllerDelegate, UIN
             present(imagePicker, animated: true, completion: nil)
         }
     }
+    @IBAction func share(_ sender: Any) {
+        
+        var items = [Any]()
+        
+      
+            items = [myImage.image!]
+        
+        let ac = UIActivityViewController(activityItems: items, applicationActivities: nil)
+        present(ac, animated: true)
+              
+    }
+    
+    
     
     // accessing photo library
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-            myImage.image = image
+        if let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            myImage.image! = image
         }
         dismiss(animated: true, completion: nil)
     }
- 
+ // save photo on UI View
+    func saveImage(imageName: String ){
+        //  create instance of FileManager
+        let fileManager = FileManager.default
+        // get the image path
+        let imagePath = (NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true) [0] as NSString).appendingPathComponent(imageName)
+        // get the image took from camera
+        let image = myImage.image!
+        // get the PNG data for image
+        let data = image.pngData()
+        // store it in the document directory
+        fileManager.createFile(atPath: imagePath as String, contents: data, attributes: nil)
+    }
     
 }
 
@@ -78,4 +110,10 @@ extension HomeViewController: UIViewControllerTransitioningDelegate {
     }
     
 
+}
+
+struct HomeViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
 }
