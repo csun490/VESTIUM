27561fe9cd3c8890cffcfc7e39c836a827ba723b
demiurge94 <erickvicencio@gmail.com>
2020-10-29 02:18:32 -0700

    added three buttons to home page, record outfits which will link to cesar's version of 'home', collage which needs to be worked on, closet which links to closet

--- VESTIUM/MENU/SlideInTransition/SlideInTransition/Menu/HomeViewController.swift
+++ VESTIUM/MENU/SlideInTransition/SlideInTransition/Menu/HomeViewController.swift
@@ -10,6 +10,8 @@ import SwiftUI
 import UIKit
 
 class HomeViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    var stackView = UIStackView()
+    var welcomeLabel = UILabel()
     
     @IBOutlet weak var myImage: UIImageView!
     
@@ -19,9 +21,71 @@ class HomeViewController: UIViewController, UIImagePickerControllerDelegate, UIN
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //configureTitleLabel()
+        //configureStackView()
         // Do any additionl setup after loading the view.
     }
-   
+ /*   func addButtonsToStackView(){
+        let newOutfitButton = UIButton()
+        let libraryButton = UIButton()
+        let createCollageButton = UIButton()
+        
+        
+        newOutfitButton.setTitle("Create a new outfit" , for: .normal)
+        libraryButton.setTitle("Library", for: .normal)
+        createCollageButton.setTitle("Collage", for: .normal)
+        newOutfitButton.setTitleColor(UIColor.red, for: .normal)
+        libraryButton.setTitleColor(UIColor.red, for: .normal)
+        createCollageButton.setTitleColor(UIColor.red, for: .normal)
+        stackView.addArrangedSubview(newOutfitButton)
+        stackView.addArrangedSubview(libraryButton)
+        stackView.addArrangedSubview(createCollageButton)
+        
+ 
+       /* let numberOfButtons = 3
+        for i in 1...numberOfButtons{
+            let button = UIButton()
+            button.setTitle("\(i) Create Collage", for: .normal)
+            stackView.addArrangedSubview(button)
+            
+        }
+ */
+    }
+    func configureStackView(){
+        view.addSubview(stackView)
+        stackView.axis          = .vertical
+        stackView.distribution  = .fillEqually
+        stackView.spacing       = 10
+        addButtonsToStackView()
+        setStackViewConstraints()
+        
+        }
+    func configureTitleLabel(){
+        view.addSubview(welcomeLabel)
+        welcomeLabel.text                       = "Welcome!"
+        welcomeLabel.font                       = UIFont.boldSystemFont(ofSize: 36)
+        welcomeLabel.textAlignment              = .center
+        welcomeLabel.numberOfLines              = 0
+        welcomeLabel.adjustsFontSizeToFitWidth  = true
+        setWelcomeLabelConstraints()
+        
+    }
+    func setWelcomeLabelConstraints(){
+        welcomeLabel.translatesAutoresizingMaskIntoConstraints = false
+        welcomeLabel.topAnchor.constraint(equalTo: view.bottomAnchor, constant: 20).isActive = true
+        welcomeLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50).isActive = true
+        welcomeLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -50).isActive = true
+        
+    }
+    func setStackViewConstraints(){
+        //2:49
+        stackView.translatesAutoresizingMaskIntoConstraints                                                           = false
+        stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive            = true
+        stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive    = true
+        stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -30).isActive     = true
+    }
+ */
     @IBAction func didTapMenu(_ sender: UIBarButtonItem) {
        guard let menuViewController = storyboard?.instantiateViewController(withIdentifier:"MenuViewController") as?
         MenuViewController else {return}
@@ -32,7 +96,6 @@ class HomeViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         menuViewController.transitioningDelegate = self
         present(menuViewController, animated: true)
     }
-    
     // camera button
     @IBAction func cameraButton(_ sender: Any) {
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
