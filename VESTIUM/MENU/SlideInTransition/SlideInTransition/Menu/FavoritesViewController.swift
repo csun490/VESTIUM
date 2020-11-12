22bebaa21967bc78e9974b2a1546f1c85f8b8fe5
Shant Melikyan <shantmelikyan@Shants-MacBook-Pro.local>
2020-11-11 19:00:34 -0800

    favourite tab image pick

--- VESTIUM/MENU/SlideInTransition/SlideInTransition/Menu/FavoritesViewController.swift
+++ VESTIUM/MENU/SlideInTransition/SlideInTransition/Menu/FavoritesViewController.swift
@@ -11,6 +11,41 @@ import UIKit
 
 class FavoritesViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    @IBOutlet weak var shirtImage: UIImageView!
+    @IBOutlet weak var pantsImage: UIImageView!
+    @IBOutlet weak var shoeImage: UIImageView!
+    var imagePicked = 0
+    
+    @IBAction func tshirtButton(_ sender: UIButton) {
+        imagePicked = 1
+        let image = UIImagePickerController()
+        image.delegate = self
+        image.sourceType = UIImagePickerController.SourceType.photoLibrary
+        image.allowsEditing = true
+        image.accessibilityRespondsToUserInteraction = true
+        self.present(image, animated: true)
+    }
+    
+    @IBAction func pantsButton(_ sender: UIButton) {
+        imagePicked = 2
+        let image = UIImagePickerController()
+        image.delegate = self
+        image.sourceType = UIImagePickerController.SourceType.photoLibrary
+        image.allowsEditing = true
+        image.accessibilityRespondsToUserInteraction = true
+        self.present(image, animated: true)
+    }
+    
+    @IBAction func shoeButton(_ sender: UIButton) {
+        imagePicked = 3
+        let image = UIImagePickerController()
+        image.delegate = self
+        image.sourceType = UIImagePickerController.SourceType.photoLibrary
+        image.allowsEditing = true
+        image.accessibilityRespondsToUserInteraction = true
+        self.present(image, animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additionl setup after loading the view.
@@ -33,4 +68,24 @@ class FavoritesViewController: UIViewController, UIImagePickerControllerDelegate
         picker.dismiss(animated: true, completion: nil)
     }
     
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+           if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage { // check image
+                if imagePicked == 1{
+                    shirtImage.image = image
+                } else if imagePicked == 2 {
+                    pantsImage.image = image
+                } else if imagePicked == 3 {
+                    shoeImage.image = image
+                } else {
+                    print("something went wrong")
+                }
+           }else {
+            print("something went wrong")
+           }
+    self.dismiss(animated: true, completion: nil)
+    }
 }
+    
+    
+
---
+++ VESTIUM/MENU/SlideInTransition/SlideInTransition/SignUpViewController 2.swift
@@ -0,0 +1,79 @@
+//
+//  SignUpViewController.swift
+//  SlideInTransition
+//
+//  Created by Mimi  on 11/8/20.
+//  Copyright © 2020 CSUN-Vestium. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+import FirebaseDatabase
+
+class SignUpViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        userNameTextField.delegate = self
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        
+    }
+    
+    // dismiss keyboard when 'return' key is pressed
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+          textField.resignFirstResponder()
+          return true
+      }
+    
+    // dismiss keyboard when user taps on view
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+
+    
+    // go to login view
+    @IBAction func goToLoginView(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+        // sign up button
+    @IBAction func signUpButton(_ sender: Any) {
+        // create new user
+        Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (result, error) in
+                   if let _eror = error {
+                       // user was not created, error
+                       print(_eror.localizedDescription)
+                   } else {
+                       //user registered successfully
+                       print(result)
+                    
+                    // firebase root reference url
+                    let ref = Database.database().reference()
+                    
+                    // create child node in root
+                    let userReference = ref.child("users")
+                    
+                    guard let userID = Auth.auth().currentUser?.uid else { return }
+                    // new user in root.child
+                    let newUserReference = userReference.child(userID)
+                    //print(newUserReference)
+                    
+                    // store username and email to database
+                    newUserReference.setValue(["username": self.userNameTextField.text!, "email": self.emailTextField.text!])
+                    print("location: \(newUserReference.description())")
+                    self.performSegue(withIdentifier: "signUpToHome", sender: nil )
+                    
+                   }
+        }
+    }
+    
+}
